0fecaf7024e7d07dd6e08842f2e7be8ec3aa8967
Mohamed Elhaj <mo.elhaj@outlook.com>
2019-01-17 19:01:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,51 @@
+// class form
+class Loan {
+    constructor(amount, interest, years) {
+        this.amount = parseFloat(amount)
+        this.interest = parseFloat(interest) / 100 / 12;
+        this.years = parseFloat(years) * 12;
+    }
+
+    calculate() {
+        var x = Math.pow(1 + this.interest, this.years);
+        var monthly = (this.amount * x * this.interest) / (x - 1);
+        var results = {
+            monthlyPayment : monthly.toFixed(2),
+            totalPrincaple : this.amount,
+            totalInterest : ((monthly * this.years) - this.amount).toFixed(2)
+        }
+        return results;
+    }
+}
+
+// get data from form
 document.querySelector('#calculateForm').addEventListener('submit', e => {
     e.preventDefault()
     document.querySelector('#calculation').innerHTML = ''
-    amount = document.querySelector('#amount')
-    interest = document.querySelector('#interest')
-    years = document.querySelector('#years')
+    var amount = document.querySelector('#amount')
+    var interest = document.querySelector('#interest')
+    var years = document.querySelector('#years')
 
-    // Calculation
+    form = new Loan(amount.value, interest.value, years.value);
+    results = form.calculate()
 
-    const principal = parseFloat(amount.value);
-    const calculatedInterest = parseFloat(interest.value) / 100 / 12;
-    const calculatedPayments = parseFloat(years.value) * 12;
-
-    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal * x * calculatedInterest) / (x - 1);
-
-    if (isFinite(monthly)) {
+    //display result
+    if(isFinite(results.monthlyPayment)){
+        document.querySelector('#error').style.display = 'none'
         document.querySelector('#calculation').innerHTML = `
-        <table class="striped">
-            <tr>
-                <td>Monthly Payment</td>
-                <td>${monthly.toFixed(2)}</td>
-            </tr>
-            <tr>
-                <td>Total Payment</td>
-                <td>${(monthly * calculatedPayments).toFixed(2)}</td>
-            </tr>
-            <tr>
-                <td>Total Interest</td>
-                <td>${((monthly * calculatedPayments) - principal).toFixed(2)}</td>
-            </tr>
-        </table>
+        <p>Monthly Payments</p>
+        <h3><strong>${results.monthlyPayment}</strong></h3>
+        <ul>
+            <li>Total Principal Paid: ${results.totalPrincaple}</li>
+            <li>Total Interest Paid: ${results.totalInterest}</li>
+        </ul>
         `
     } else {
         document.querySelector('#error').style.display = 'block'
         document.querySelector('#error').innerText = 'Invalid Numbers'
     }
+
+
+    //reset the form
+    document.getElementById("calculateForm").reset();
 })
